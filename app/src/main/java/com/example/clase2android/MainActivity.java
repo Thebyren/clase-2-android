73d00c295890deafd460e0763f6112cf867bdbd3
Thebyren <byronrena1@gmail.com>
2025-08-23 11:27:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/clase2android/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.clase2android;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_crear_db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btn_crear_db = findViewById(R.id.btnCrearDb);
+        btn_crear_db.setOnClickListener(v -> {
+            Toast.makeText(this, "Crear base de datos", Toast.LENGTH_LONG).show();
+        });
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
